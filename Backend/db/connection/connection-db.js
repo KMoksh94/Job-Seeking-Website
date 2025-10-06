2222bbe141073a0359a0cc28d3cbe6da3f1f9945
Mokshit Kanojia <kmoksh94@gmail.com>
2025-10-06 22:15:07 +0530

    removed connection link for safety

--- Backend/db/connection/connection-db.js
+++ Backend/db/connection/connection-db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 module.exports = async function connectionDB(){
   try {
-    const res = await mongoose.connect("mongodb+srv://kmoksh94_db_user:kmoksh94Assignment1@cluster0.4dibjog.mongodb.net/")
+    const res = await mongoose.connect(process.env.connection_link)
     console.log(`Connected to Database`);
   } catch (error) {
     console.log(error);
